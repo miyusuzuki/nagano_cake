4f6e3093db08252aaffd6153cf26a6cbbfb1ad82
miyusuzuki <107457233+miyusuzuki@users.noreply.github.com>
2022-08-06 14:02:11 +0000

    [10 commit]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+ def after_sign_in_path_for(resource)
+        case resource
+        when Admin
+            root_path
+        when Customer
+            customers_my_page_path # ログイン後に遷移するpathを設定
+        end
+  end 
+  
   private
 
     def configure_permitted_parameters
