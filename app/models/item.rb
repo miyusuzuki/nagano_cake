4ceef14581bc1782f9dd810775463d190ed19638
miyusuzuki <107457233+miyusuzuki@users.noreply.github.com>
2022-08-09 07:09:38 +0000

    [13 commit]

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
+  has_many :ordered_products, dependent: :destroy
+  belongs_to :genre
   has_one_attached :image
   def add_tax_price
         (self.price * 1.08).round
--- app/models/ordered_product.rb
+++ app/models/ordered_product.rb
@@ -1,6 +1,6 @@
 class OrderedProduct < ApplicationRecord
-  belongs_to :items
-  belongs_to :order_informations
+  belongs_to :item
+  belongs_to :order_information
   
   enum making_status: {製作不可: 0, 製作待ち: 1, 製作中: 2, 製作完了: 3}
 end
