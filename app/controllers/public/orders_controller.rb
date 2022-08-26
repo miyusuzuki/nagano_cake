a84718e33b2b2685d9e92783169092e01d42dcea
miyusuzuki <107457233+miyusuzuki@users.noreply.github.com>
2022-08-26 17:41:54 +0000

    [17 commit]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -30,28 +30,54 @@ class Public::OrdersController < ApplicationController
      else 
       session[:user][:shipping_postal_code] =  params[:shipping_postal_code]
       session[:user][:shipping_address] = params[:shipping_address]
-      session[:user][:shipping_name] = params[:shipping_name]  
-     end
-  end
+      session[:user][:shipping_name] = params[:shipping_name]
+     end  
+	end
+	
   def complete
-    order_information = OrderInformation.new(session[:user])
-    order_information.customer_id = current_customer.id
-    order_information.save
-    cart_items = current_customer.cart_items
-    @cart_items.each do |cart_item|
-      ordered_item = OrderedItem.new
-      ordered_item.item_id = cart_item.item.id
-      ordered_item.status = 0
-      ordered_item.price = cart_item.item.add_tax_price
-      ordered_item.quantity = cart_item.amount
-      ordered_item.order_id = order.id
-      ordered_item.save
-    end
-    @cart_items.destroy_all
   end
   
   def create
-    
+     @cart_items = CartItem.where(customer_id:[current_customer.id])
+     @order_information = OrderInformation.new
+     session[:user] = OrderInformation.new()
+    #支払方法
+     if params[:method_of_payment_select] == "0"
+      session[:user][:method_of_payment] = 0
+     elsif params[:method_of_payment_select] == "1"
+      session[:user][:method_of_payment] = 1
+     end
+    #配送先
+     if params[:address_select] == "0"
+      session[:user][:shipping_postal_code] = current_customer.postal_code
+      session[:user][:shipping_address] = current_customer.address
+      session[:user][:shipping_name] = current_customer.name
+     elsif params[:address_select] == "1"
+      session[:user][:shipping_postal_code] = Address.find(params[:address_id]).postal_code
+      session[:user][:shipping_address] = Address.find(params[:address_id]).address
+      session[:user][:shipping_name] = Address.find(params[:address_id]).name
+     else 
+      session[:user][:shipping_postal_code] =  params[:shipping_postal_code]
+      session[:user][:shipping_address] = params[:shipping_address]
+      session[:user][:shipping_name] = params[:shipping_name]  
+     end
+     @order_information = OrderInformation.new
+       @order_information.save
+    # 以下、ordered_product作成
+		@cart_items = current_customer.cart_items
+		@cart_items.each do |cart_item|
+			@ordered_product = OrderedProduct.new
+			@ordered_product.order_informations_id = @order_information.id
+			@ordered_product.item_id = cart_item.item.id
+			@ordered_product.amount = cart_item.amount
+			@ordered_product.making_status = 0
+			@ordered_product.price = cart_item.item.add_tax_price.floor
+			@ordered_product.save
+		end
+
+		# 購入後はカート内商品削除
+		@cart_items.destroy_all
+		redirect_to orders_complete_path
   end
   
   def index
@@ -62,8 +88,8 @@ class Public::OrdersController < ApplicationController
   
 private
 
-  def order_params
-    params.require(:order).permit(:shipping_name, :shipping_postal_code, :shipping_address, :method_of_payment, :customer_id)
+  def order_information_params
+    params.require(:order_information).permit(:shipping_name, :shipping_postal_code, :shipping_address, :method_of_payment, :customer_id)
   end
   
 end  
