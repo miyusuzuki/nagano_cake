360f0ebe2d2327574c0b57ad998a5809fb8f5773
miyusuzuki <107457233+miyusuzuki@users.noreply.github.com>
2022-08-27 20:19:45 +0000

    [18 commit]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -31,7 +31,7 @@ class Public::OrdersController < ApplicationController
       session[:user][:shipping_postal_code] =  params[:shipping_postal_code]
       session[:user][:shipping_address] = params[:shipping_address]
       session[:user][:shipping_name] = params[:shipping_name]
-     end  
+     end
 	end
 	
   def complete
@@ -39,48 +39,33 @@ class Public::OrdersController < ApplicationController
   
   def create
      @cart_items = CartItem.where(customer_id:[current_customer.id])
-     @order_information = OrderInformation.new
-     session[:user] = OrderInformation.new()
-    #支払方法
-     if params[:method_of_payment_select] == "0"
-      session[:user][:method_of_payment] = 0
-     elsif params[:method_of_payment_select] == "1"
-      session[:user][:method_of_payment] = 1
-     end
-    #配送先
-     if params[:address_select] == "0"
-      session[:user][:shipping_postal_code] = current_customer.postal_code
-      session[:user][:shipping_address] = current_customer.address
-      session[:user][:shipping_name] = current_customer.name
-     elsif params[:address_select] == "1"
-      session[:user][:shipping_postal_code] = Address.find(params[:address_id]).postal_code
-      session[:user][:shipping_address] = Address.find(params[:address_id]).address
-      session[:user][:shipping_name] = Address.find(params[:address_id]).name
-     else 
-      session[:user][:shipping_postal_code] =  params[:shipping_postal_code]
-      session[:user][:shipping_address] = params[:shipping_address]
-      session[:user][:shipping_name] = params[:shipping_name]  
-     end
-     @order_information = OrderInformation.new
-       @order_information.save
-    # 以下、ordered_product作成
-		@cart_items = current_customer.cart_items
-		@cart_items.each do |cart_item|
+     @order_information = OrderInformation.new(session[:user])
+     @order_information.postage = 800
+     @order_informations.billing_amount = 0
+     @order_information.customer_id = current_customer.id
+     @order_information.save
+    # 以下、order_detail作成
+		 @cart_items = current_customer.cart_items
+		 @cart_items.each do |cart_item|
 			@ordered_product = OrderedProduct.new
-			@ordered_product.order_informations_id = @order_information.id
+			@ordered_product.order_information_id = @order_information.id
 			@ordered_product.item_id = cart_item.item.id
 			@ordered_product.amount = cart_item.amount
 			@ordered_product.making_status = 0
-			@ordered_product.price = cart_item.item.add_tax_price.floor
+			@ordered_product.price = cart_item.item.add_tax_price
 			@ordered_product.save
 		end
 
 		# 購入後はカート内商品削除
 		@cart_items.destroy_all
 		redirect_to orders_complete_path
-  end
+		end
+
+		
   
   def index
+    @order_informations = OrderInformation.where(customer_id:[current_customer.id])
+    @ordered_products = OrderedProduct.where(customer_id:[current_customer.id])
   end
   
   def show
